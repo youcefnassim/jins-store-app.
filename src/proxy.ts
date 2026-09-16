2a9f83a8455d3cefa62e3f0945bd93f8439077bb
youcefnassim <youcefnassim@example.com>
2026-09-16 23:27:45 +0100

    fix: rename middleware to proxy for Next.js 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,5 +7,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)',]
 };
